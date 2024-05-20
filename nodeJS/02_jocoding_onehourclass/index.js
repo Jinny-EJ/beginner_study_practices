fa17727858cc41d6ee2712e62ac4dc54388b8d80
EunJin SHIN <ghghgh0202@gmail.com>
2024-05-20 13:18:16 +0900

    study

--- nodeJS/02_jocoding_onehourclass/index.js
+++ nodeJS/02_jocoding_onehourclass/index.js
@@ -1,1 +1,32 @@
-console.log("Hello world!")
+const express = require("express");
+const app = express();
+var cors = require('cors')
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/sound/:name', (req,res) => {
+    const { name } = req.params
+
+    if(name == 'dog') {
+        res.json({'sound': '멍멍' })
+    } else if(name == "cat") {
+        res.json({'sound': '야옹' })
+    } else if(name == "pig") {
+        res.json({'sound': '꿀꿀' })
+    } else {
+        res.json({'sound': '알수없음'})
+    }
+})
+
+app.get("/cat", (req, res) => {
+  res.send("고양이");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
