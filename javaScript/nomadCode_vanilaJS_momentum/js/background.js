db42c2939230dd7d5b12dbaacf86e762c851928c
Jinny_EJ <ghghgh0202@gmail.com>
2024-05-30 13:19:32 +0900

    vite 시작

--- javaScript/nomadCode_vanilaJS_momentum/js/background.js
+++ javaScript/nomadCode_vanilaJS_momentum/js/background.js
@@ -5,7 +5,8 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 
 const bgImage = document.createElement("img");
-
+bgImage.classList.add("background_image");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage); // body 에 html 을 추가하는 것 = appendChild
+
