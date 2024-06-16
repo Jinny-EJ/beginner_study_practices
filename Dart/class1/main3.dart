635f556230d8d267165f700cfb2c57cf38bf6cbd
Jinny_EJ <ghghgh0202@gmail.com>
2024-06-16 13:50:37 +0900

    Dart study

--- Dart/class1/main3.dart
+++ Dart/class1/main3.dart
@@ -6,15 +6,29 @@ class Human {
   }
 }
 
+class Strong {
+  final double strengthLevel = 1500.99;
+}
+
+// 생성자가 없는 class 만듬.
+class QuickRunner {
+  void runQuick() {
+    print("ruuuuuuuun!");
+  }
+}
+
+class Tall {
+  final double height = 1.99;
+}
+
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
   Player({
     required this.team,
-    required String name,
-  }) : super(name);
+  });
 
   @override
   void sayHello() {
@@ -22,6 +36,10 @@ class Player extends Human {
     print('and I play for ${team}');
   }
 }
+
+class Horse with Strong,QuickRunner, {}
+
+class Kid with QuickRunner{}
 
 void main() {
   var player = Player(team: Team.red, name: 'nico');
