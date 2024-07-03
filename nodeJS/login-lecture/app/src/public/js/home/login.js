ca8b40cb0472f1e93768041d561e1dd6808ca8c5
Jinny_EJ <ghghgh0202@gmail.com>
2024-07-03 17:39:20 +0900

    로그인을 요청하는 fetch 생성.

--- nodeJS/login-lecture/app/src/public/js/home/login.js
+++ nodeJS/login-lecture/app/src/public/js/home/login.js
@@ -13,4 +13,12 @@ function login() {
         psword: psword.value,
     };
     console.log(req);
+
+    fetch ("/login", {
+        method: "POST", // html API 에 있는 method 중에 POST 사용. 
+        headers: { 
+            "Content-Type": "application/json"
+        }, // 내가 보내는 데이터가 JSON 데이터라고 명시적으로 말하는 것. 
+        body: JSON.stringify(req), // 그냥 보내는 것과 JSON 으로 보내는 것은 차이가 있는데, JSON 은 텍스트 형식으로 보내게 됨. 그냥 보낼때는 객체 형식 
+    });
 };
--- nodeJS/login-lecture/app/src/routes/home/index.js
+++ nodeJS/login-lecture/app/src/routes/home/index.js
@@ -7,4 +7,5 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.hello);
 router.get("/login", ctrl.login);
 
+
 module.exports = router;
