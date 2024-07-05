8ecc1b4fb28e7ae6801f206e05dcffefedd65e86
Jinny_EJ <ghghgh0202@gmail.com>
2024-07-05 12:26:20 +0900

    회원가입페이지 구현

--- nodeJS/login-lecture/app/src/routes/home/index.js
+++ nodeJS/login-lecture/app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 // API 들 
 router.get("/", ctrl.output.hello);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login); // 프론트 앤드가 전달한 로그인 데이터를 받아서 로그인을 처리해주는 API
 
 
