72bbb2c6f8ff6d7fce4a546f35aed2dd3f56af38
Jinny_EJ <ghghgh0202@gmail.com>
2024-07-06 22:05:49 +0900

    database begin

--- nodeJS/login-lecture/app/src/models/UserStorage.js
+++ nodeJS/login-lecture/app/src/models/UserStorage.js
@@ -1,15 +1,15 @@
 "use strict";
 
+// users.json 에서 파일을 읽어오게 하려면 파일시스템이 필요. 
+const fs = require("fs").promises;
+
+
 
 class UserStorage {
-    static #users = { // static 을 붙이면 정적변수가 됨. #를 넣어주면 private 한 경우로 지정됨. 
-        id: ["woorimIT", "나개발", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name: ["우리밋","나개발","김팀장"],
-      };
+    // static #users = ;
 
     static getUsers(...fields){
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers = fields.reduce((newUsers,field) => { // 여기서 reduce 는 반복문
             if (users.hasOwnProperty(field)) {
                 newUsers[field] = users [field];
@@ -20,19 +20,27 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        },{});
-
-        return userInfo; 
+        console.log(fs.readFile("./src/databases/users.json")); 
+        
+        
+        
+        
+        // (err,data) => {
+        //     if (err) throw err;
+        //     const users = JSON.parse(data);
+        //     const idx = users.id.indexOf(id);
+        //     const usersKeys = Object.keys(users);
+        //     const userInfo = usersKeys.reduce((newUser, info) => {
+        //         newUser[info] = users[info][idx];
+        //         return newUser;
+        //     },{});
+    
+        //     return userInfo; 
+        // });
     }
 
     static save(userInfo) {
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
