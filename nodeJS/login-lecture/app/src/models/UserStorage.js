74e85ddff97978475827e30cd7812e6a80b1d2aa
Jinny_EJ <ghghgh0202@gmail.com>
2024-07-17 21:02:39 +0900

    수정사항

--- nodeJS/login-lecture/app/src/models/UserStorage.js
+++ nodeJS/login-lecture/app/src/models/UserStorage.js
@@ -6,7 +6,7 @@ const fs = require("fs").promises;
 
 class UserStorage {
 
-  static #getUserInfo() {
+  static #getUserInfo(data, id) {
     const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const usersKeys = Object.keys(users); // => [id, psword, name]
