f740cda112449a078fe22a0cb63ba046644109f7
Jinny_EJ <ghghgh0202@gmail.com>
2024-07-18 15:47:35 +0900

    AWS RDS 와 연동 & dotenv로 환경변수 등록

--- nodeJS/login-lecture/app/src/models/UserStorage.js
+++ nodeJS/login-lecture/app/src/models/UserStorage.js
@@ -1,69 +1,31 @@
 "use strict";
 
-// users.json 에서 파일을 읽어오게 하려면 파일시스템이 필요.
-const fs = require("fs").promises;
-// readFile 자체에서 Promise 를 제공하고 있음. 이 기능을 가져와서 사용하려면 위 처럼 .promises 를 적어주면 됨.
+
+const db = require("../config/db"); 
 
 class UserStorage {
-
-  static #getUserInfo(data, id) {
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // => [id, psword, name]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
-  }
-
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) return users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
-  static getUsers(isAll, ...fields) {
-    return fs
-      .readFile("./src/databases/users.json")
-      .then((data) => {
-        return this.#getUsers(data, isAll, fields);
-      })
-      .catch(console.error);
-  }
-
   static getUserInfo(id) {
-    return (
-      fs
-        .readFile("./src/databases/users.json") // 그리고 fs 가 promises 를 반환하게 됨. 유지보수 및 가독성을 높여준다.
-        //promise 를 반환을 하게 되면, then 이라는 메서드를 사용할 수 있게 된다. 그리고 promise 에 대한 오류처리는 catch() 로 할 수 있게 됨.
-        // 프론트앤드에서 서버에 자료를 요청할때 사용했던 fetch API 가 바로 promise 를 반환하는 것인데, 그것과 동일하다고 생각하면 됨.
-        // .then() 은 fs.readFile () 안의 것이 성공했을때 실행, .catch()는 fs.readFile () 안의 것이 실패했을 때 실행.
-
-        .then((data) => {
-          return this.#getUserInfo(data, id);
-        })
-
-        .catch(console.error)
-    );
+    return new Promise((resolve,reject) => {
+      const query = "SELECT *FROM users WHERE id = ?;";
+      db.query(query, [id],(err,data) => {
+        if (err) reject (`${err}`);
+        resolve(data[0]);
+      }); 
+    });
   }
 
   static async save(userInfo) {
-    const users = await this.getUsers(true);
-    if (users.id.includes(userInfo.id)) {
-      throw "이미 존재하는 아이디입니다.";
-    }
-    users.id.push(userInfo.id);
-    users.name.push(userInfo.name);
-    users.psword.push(userInfo.psword);
-    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
-    return { success: true };
+    return new Promise((resolve,reject) => {
+      const query = "INSERT INTO users(id,name,psword) VALUES(?, ?, ?);";
+      db.query(
+        query, 
+        [userInfo.id, userInfo.name, userInfo.psword],
+        (err) => {
+          if (err) reject (`${err}`);
+          resolve({success: true});
+        }
+      );
+    });
   }
 }
 
