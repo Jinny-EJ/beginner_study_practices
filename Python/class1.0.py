849007d636b8778982d4e8346c7a197815038faa
Jinny_EJ <ghghgh0202@gmail.com>
2025-07-08 22:13:09 -0600

    Youtube

---
+++ Python/class1.0.py
@@ -0,0 +1,58 @@
+# 자료형 
+
+# 숫자형 
+print(5) #여기서 print 라는 함수를 사용함. 
+print(-10) #음수
+print(3.14) #실수
+print(5+3) #연산
+print(2*3) #곱셈
+print(2+(3*4)) #좀 더 복잡한 연산 
+
+
+# 문자열 자료형 
+print('풍선') # 작은 따옴표나 큰 따옴표 모두 사용 가능. 
+print("나비") 
+print('ㅋㅋㅋㅋㅋㅋㅋ')
+print('ㅋ'*9) # 이렇게도 표현 가능. 
+
+
+# Boolean 자료형 : 참 / 거짓 
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+
+
+# 변수 
+animal = "강아지"
+
+station = "신도림"
+print( station + "행 열차가 들어오고 있습니다.")
+
+# 연산 
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10 
+print(6/3) # 2
+
+print(2**3) # 2ˆ3 = 8
+print(5%3) # 나머지 구하기 
+print(10%3) # 1
+print(5//3) # 1
+print(10//3) # 3
+
+print(10 > 3) # True 
+print(4 >= 7) # False 
+print(10 <3) # False 
+print(5 <= 5) # True
+
+print(1 != 3) # True 
+print(not(1 != 3)) # False 
+
+print((3>0) and (3 < 5)) # True 
+print((3 >0) & (3 <5)) # True 
+
+print(5 > 4 > 3) # True 
+print(5 > 4 > 7) # False 
+
